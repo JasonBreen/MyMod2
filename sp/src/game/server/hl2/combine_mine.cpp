95b331f8d4af55e6c2d4a048cef19f7b4d742294
Sam Bianchi <dr.freeman879@gmail.com>
2019-11-23 18:28:28 -0500

    Update combine_mine.cpp

--- sp/src/game/server/hl2/combine_mine.cpp
+++ sp/src/game/server/hl2/combine_mine.cpp
@@ -1,6 +1,6 @@
 //========= Copyright Valve Corporation, All rights reserved. ============//
 //
-// Purpose: 
+// Purpose: Combine hopper mine script
 //
 //=============================================================================//
 
